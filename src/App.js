9c293d47f97c1afb831ab4bb6963f480852dca10
samir-missaoui <samirmissaoui31@gmail.com>
2021-10-28 11:20:49 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./style.css";
+import mybestImage from "./téléchargement (1).png";
+import mybestvideo from "./CSSFlexbox.mp4";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          border: "solid",
+          borderWidth: 6,
+          borderColor: "black",
+          bordermaxwidth: "100vw",
+        }}
+      >
+        <h1 className="title red">
+          Your name here <br />
+        </h1>
+        <img src="/téléchargement.png" alt="myimage" />
+        <br />
+        <img src={mybestImage} alt="myImage" />
+      </div>
+      <video src={mybestvideo} width="320" height="240" controls="controls" />
     </div>
   );
 }
